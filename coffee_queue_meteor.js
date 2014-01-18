f3f252c4ec1119248c064fe6cdc16cb2f470ee02
Andy Maleh <andy.am@gmail.com>
2014-01-18 18:03:47 -0500

    Progress on CoffeeQueue (one text field a the moment) in Meteor

--- coffee_queue_meteor.js
+++ coffee_queue_meteor.js
@@ -1,19 +1,28 @@
+Orders = new Meteor.Collection("orders");
+
 if (Meteor.isClient) {
-  Template.hello.greeting = function () {
+  Template.order.greeting = function () {
     return "Welcome to coffee_queue_meteor.";
   };
 
-  Template.hello.events({
-    'click input' : function () {
+//  Template.coffeeQueue.products = function() { ["Mocha", "Americano"] }
+
+  Template.order.events({
+    'click input[type=button]' : function (element) {
       // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
+      if (typeof console !== 'undefined') {
+        Orders.insert({name: $('input[type=text]').val()});
+        console.log("Order has been placed");
+      }
     }
   });
+
+  Template.orders.orders = function() {
+    return Orders.find();
+  }
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    // code to run on server at startup
   });
 }
